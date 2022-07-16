1969a2961d241bb96c8c63a8f5cb1b8028bd7f05
eli-shi <shirinyan.elina@gmail.com>
2022-07-16 20:37:06 +0400

    get index from list, clean code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -39,61 +39,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-                drawable = getResources().getDrawable(R.drawable.banner);
+        drawable = getResources().getDrawable(R.drawable.banner);
 
-                Button search = (Button) findViewById(R.id.button);
-                EditText inputName = (EditText) findViewById(R.id.nameInput);
-                EditText inputSurname = (EditText) findViewById(R.id.surnameInput);
-                TextView outputSchedule = (TextView) findViewById(R.id.schedule);
+        Button search = (Button) findViewById(R.id.button);
+        EditText inputName = (EditText) findViewById(R.id.nameInput);
+        EditText inputSurname = (EditText) findViewById(R.id.surnameInput);
+        TextView outputSchedule = (TextView) findViewById(R.id.schedule);
 
         ArrayList<Student> studentsList = new ArrayList<Student>();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("/student");
         myRef.addValueEventListener(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(DataSnapshot dataSnapshot) {
-                                            for (DataSnapshot item_snapshot : dataSnapshot.getChildren()) {
-                                                Student student = item_snapshot.getValue(Student.class);
-                                                studentsList.add(student);
-                                                Log.i("students", student.name);
-                                            }
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot item_snapshot : dataSnapshot.getChildren()) {
+                    Student student = item_snapshot.getValue(Student.class);
+                    studentsList.add(student);
+                    Log.i("students", student.name);
+                }
 
-                                        }
-
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError error) {
-
-                                        }
-                                    }
-        );
-
-
-        search.setOnClickListener(
-                        new View.OnClickListener() {
-                            @RequiresApi(api = Build.VERSION_CODES.N)
-                            public void onClick(View view) {
-                                Log.v("Name", inputName.getText().toString());
-                                Log.v("Surname", inputSurname.getText().toString());
-                                if (inputSurname.getText().toString().isEmpty() || inputName.getText().toString().isEmpty()) {
-                                    Toast.makeText(view.getContext(), "please input all the required information", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
-                                boolean studentFound = false;
-                                for (int index = 0; index < studentsList.size(); index++) {
-                                    if (studentsList.get(index).name.equals(inputName.getText().toString()) && studentsList.get(index).surname.equals(inputSurname.getText().toString())) {
-                                        Student student = studentsList.get(index);
-                                        outputSchedule.setText(getFormattedSchedule(student.getSchedule(), 0, 0));
-                                        studentFound = true;
-                                    }
-                                }
-                                if (studentFound == false) {
-                                    Toast.makeText(view.getContext(), "This student does not exist", Toast.LENGTH_SHORT).show();
-                                }
-
-                            }
-                        });
             }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            public void onClick(View view) {
+                Log.v("Name", inputName.getText().toString());
+                Log.v("Surname", inputSurname.getText().toString());
+                if (inputSurname.getText().toString().isEmpty() || inputName.getText().toString().isEmpty()) {
+                    Toast.makeText(view.getContext(), "please input all the required information", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                boolean studentFound = false;
+                for (int index = 0; index < studentsList.size(); index++) {
+                    if (studentsList.get(index).name.equals(inputName.getText().toString()) && studentsList.get(index).surname.equals(inputSurname.getText().toString())) {
+                        Student student = studentsList.get(index);
+                        outputSchedule.setText(getFormattedSchedule(student.getSchedule(), 0, 0));
+                        studentFound = true;
+                    }
+                }
+                if (studentFound == false) {
+                    Toast.makeText(view.getContext(), "This student does not exist", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
 
     private String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 
@@ -104,17 +102,13 @@ public class MainActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.N)
     private String getFormattedSchedule(List<List<String>> schedule, int rowIndex, int collumIndex) {
 
-        String[][] scheduleArray = schedule.stream()
-                .map(l -> l.toArray(new String[l.size()]))
-                .toArray(String[][]::new);
-
         for (rowIndex = 0; rowIndex < 5; rowIndex++) {
             beauty = beauty + weekdays[rowIndex] + " - ";
             for (collumIndex = 0; collumIndex < 5; collumIndex++) {
                 if (collumIndex != 4) {
-                    beauty = beauty + scheduleArray[rowIndex][collumIndex] + ", ";
+                    beauty = beauty + schedule.get(rowIndex).get(collumIndex) + ", ";
                 } else {
-                    beauty = beauty + scheduleArray[rowIndex][collumIndex] + "\n";
+                    beauty = beauty + schedule.get(rowIndex).get(collumIndex) + "\n";
                 }
             }
         }
